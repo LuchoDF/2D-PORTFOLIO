2329b60d817876f2b65a472d0cac8a9f255aa03a
luchodf <luchodellafelice@hotmail.com>
2024-05-06 22:49:29 +0200

    Modify object dialogs

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,24 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+  pc: `This is my PC. I'm working on several projects in Typescript/Javascript as part of my Full Stack web developer program at
+  <a href="https://simplon.co/formation/concepteur-developpeur-d-applications/12" target="_blank">Simplon</a>.
+    I've also been working on my CSS and HTML these days by following <a href="https://www.theodinproject.com/" target="_blank">The Odin Project</a> Foundations course. 
+    For more information about my projects, here is my <a href="https://github.com/LuchoDF" target="_blank">Github</a>!`,
+  tv: `That's my TV. I usally watch science fiction series and films. Christopher Nolan is my favourite director.
+    I also spend quite some time on youtube watching science/educational channels like: 
+   <a href="https://www.youtube.com/@veritasium" target="_blank">Veritasium</a>, <a href="https://www.youtube.com/@freecodecamp" target="_blank">Free Code Camp</a>,
+   <a href="https://www.youtube.com/@Vsauce" target="_blank">Vsauce</a>, and comedy/entertainment channels like : <a href="https://www.youtube.com/@AisladosElPodcast" target="_blank">AisladosPodcast</a>
+    and <a href="https://www.youtube.com/@CorridorCrew" target="_blank">CorridorCrew</a> among others.`,
+  bed: `This where I sleep, wether at night or whenever I need to take a power nap. A good place to be instropective`,
+  cv: `This is my CV. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+  Contact me at luchodellafelice@hotmail.com if you have any interesting job opportunities!`,
+  desk: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. `,
+  library1: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  library2: ``,
+  sofa: `where i sit`,
+  sofaChair: `where i sit`,
+  sport: `sport`,
 };
