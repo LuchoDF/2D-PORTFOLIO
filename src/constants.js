6705472be154031593cb9106d21b3792b2627d8c
luchodf <luchodellafelice@hotmail.com>
2024-05-15 23:06:15 +0200

    Correct grammar

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const dialogueData = {
   <a href="https://simplon.co/formation/concepteur-developpeur-d-applications/12" target="_blank">Simplon</a>.
     I've also been working on my CSS and HTML these days by following <a href="https://www.theodinproject.com/" target="_blank">The Odin Project</a> Foundations course. 
     For more information about my projects, here is my <a href="https://github.com/LuchoDF" target="_blank">Github</a>!`,
-  tv: `That's my TV. I usally watch science fiction series and films. Christopher Nolan is my favourite director.
+  tv: `That's my TV. I usually watch science fiction series and films. Christopher Nolan is my favourite director.
     I also spend quite some time on youtube watching science/educational channels like: 
    <a href="https://www.youtube.com/@veritasium" target="_blank">Veritasium</a>, <a href="https://www.youtube.com/@freecodecamp" target="_blank">Free Code Camp</a>,
    <a href="https://www.youtube.com/@Vsauce" target="_blank">Vsauce</a>, and comedy/entertainment channels like : <a href="https://www.youtube.com/@AisladosElPodcast" target="_blank">AisladosPodcast</a>
@@ -19,13 +19,13 @@ export const dialogueData = {
   Contact me at luchodellafelice@hotmail.com if you have any interesting job opportunities!`,
   desk: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. `,
-  library1: `This is my library. I mostly read novels and I try to switch between spanish, english and french as
-  I like learning/improving languages. Soon Italian will be added to the list!`,
-  library2: `This is my library. I mostly read novels and I try to switch between spanish, english and french as
-  I like learning/improving languages. Soon Italian will be added to the list!`,
+  library1: `This is my library. I mostly read novels and I try to switch between Spanish, English and French as
+  I like learning/improving my language skills. Soon Italian will be added to the list!`,
+  library2: `This is my library. I mostly read novels and I try to switch between Spanish, English and French as
+  I like learning/improving my language skills. Soon Italian will be added to the list!`,
   sofa: `Comfortable place to watch TV`,
   sofaChair: `Perfect to sit on and read something`,
-  sport: `This symbolizes a place to do sports. I like to practice Calisthenics and go swimming`,
+  sport: `This symbolizes a place to exercise. I like to practice Calisthenics and go swimming`,
 };
 
 
